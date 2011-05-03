a74142a0f8154da92667eff96b95f11d7ec5e788
tofuatjava <tofuatjava@dev.java.net>
2011-05-03 18:39:55 +0000

    * JENKINS-9232: add default minimum width for testresult bar * temporary remove history data because of performance problems with huge result data

--- src/main/java/hudson/plugins/sctmexecutor/publisher/SCTMTestResult.java
+++ src/main/java/hudson/plugins/sctmexecutor/publisher/SCTMTestResult.java
@@ -2,51 +2,55 @@ package hudson.plugins.sctmexecutor.publisher;
 
 import hudson.Util;
 
+import org.apache.commons.lang.mutable.MutableDouble;
+
 public class SCTMTestResult {
   public static enum TestState {
-    FAILED("failed"),
-    SKIPPED("skipped"),
-    PASSED("passed"),
-    NOTESTS("no tests found");
-    
+    FAILED("failed"), SKIPPED("skipped"), PASSED("passed"), NOTESTS("no tests found");
+
     private String state;
-    
+
     private TestState(String state) {
       this.state = state;
     }
-    
+
     @Override
     public String toString() {
       return state;
     }
-    
+
     public String getMessage() {
       return state;
     }
   }
-  
+
+  private static final int MIN_BAR_WIDTH = 5;
+
   private TestState state = TestState.NOTESTS;
   private int failCount;
   private int skipCount;
   private int passCount;
   private float duration;
-  private String errorMessage;
-  
+  private final String errorMessage;
+  private transient int passWidth;
+  private transient int skipWidth;
+  private transient int failWidth;
+
   public SCTMTestResult(TestState state, float duration, String errormsg) {
     super();
     this.state = state;
     this.duration = duration;
     this.errorMessage = errormsg;
-    
+
     switch (state) {
-      case PASSED:
-        passCount = 1;
-        break;
-      case SKIPPED:
-        skipCount = 1;
-        break;
-      case FAILED:
-        failCount = 1;
+    case PASSED:
+      passCount = 1;
+      break;
+    case SKIPPED:
+      skipCount = 1;
+      break;
+    case FAILED:
+      failCount = 1;
     }
   }
 
@@ -56,7 +60,7 @@ public class SCTMTestResult {
     this.passCount = value.getPassCount();
     this.duration = value.getDuration();
     this.errorMessage = value.getErrorMessage();
-    
+
     calculateState();
   }
 
@@ -70,7 +74,39 @@ public class SCTMTestResult {
     else
       state = TestState.NOTESTS;
   }
-  
+
+  public void calculateBar() {
+    MutableDouble failWidth = calculateWidth(this.failCount);
+    MutableDouble skipWidth = calculateWidth(this.skipCount);
+    MutableDouble passWidth = calculateWidth(this.passCount);
+
+    modifyWidthUsingMinWidth(failWidth, skipWidth, passWidth);
+    modifyWidthUsingMinWidth(skipWidth, failWidth, passWidth);
+    modifyWidthUsingMinWidth(passWidth, failWidth, skipWidth);
+
+    this.failWidth = failWidth.intValue();
+    this.skipWidth = skipWidth.intValue();
+    this.passWidth = passWidth.intValue();
+  }
+
+  private void modifyWidthUsingMinWidth(MutableDouble toModify, MutableDouble value2, MutableDouble value3) {
+    if (toModify.doubleValue() > 0 && toModify.doubleValue() < MIN_BAR_WIDTH) {
+      double diffWidth = MIN_BAR_WIDTH - toModify.doubleValue();
+      if (value2.doubleValue() < value3.doubleValue())
+        value3.subtract(diffWidth);
+      else
+        value2.subtract(diffWidth);
+      toModify.setValue(MIN_BAR_WIDTH);
+    }
+  }
+
+  private MutableDouble calculateWidth(int testCount) {
+    if (getTotalCount() <= 0)
+      return new MutableDouble(0);
+    double value = testCount * 100 / (double) getTotalCount();
+    return new MutableDouble(value);
+  }
+
   synchronized void addSubTestCounts(int passed, int skipped, int failed) {
     this.passCount += passed;
     this.skipCount += skipped;
@@ -81,26 +117,38 @@ public class SCTMTestResult {
   synchronized void addSubDuration(float duration) {
     this.duration += duration;
   }
-  
+
   @Override
   public String toString() {
     return String.format("%s in %fms", this.state, this.duration);
   }
 
   public int getTotalCount() {
-    return failCount+skipCount+passCount;
+    return failCount + skipCount + passCount;
   }
 
   public synchronized int getFailCount() {
     return failCount;
   }
-  
+
   public synchronized int getSkipCount() {
     return skipCount;
   }
-  
+
   public synchronized int getPassCount() {
     return passCount;
+  }
+
+  public synchronized int getFailWidth() {
+    return failWidth;
+  }
+
+  public synchronized int getSkipWidth() {
+    return skipWidth;
+  }
+
+  public synchronized int getPassWidth() {
+    return passWidth;
   }
 
   public TestState getState() {
@@ -110,9 +158,9 @@ public class SCTMTestResult {
   public synchronized float getDuration() {
     return duration;
   }
-  
+
   public String getDurationString() {
-    return Util.getTimeSpanString((long)duration);
+    return Util.getTimeSpanString((long) duration);
   }
 
   public String getErrorMessage() {
---
+++ src/test/java/hudson/plugins/sctmexecutor/publisher/TestSCTMTestResult.java
@@ -0,0 +1,145 @@
+package hudson.plugins.sctmexecutor.publisher;
+
+import hudson.plugins.sctmexecutor.publisher.SCTMTestResult.TestState;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestSCTMTestResult {
+
+  private static SCTMTestResult dummyResult;
+
+  @BeforeClass
+  public static void setUp() throws Exception {
+    dummyResult = new SCTMTestResult(TestState.NOTESTS, 0, "");
+  }
+
+  @Test
+  public void testEmptyTestResult() throws Exception {
+    SCTMTestResult result = dummyResult;
+
+    Assert.assertEquals(0, result.getFailWidth());
+    Assert.assertEquals(0, result.getSkipWidth());
+    Assert.assertEquals(0, result.getPassWidth());
+    Assert.assertEquals(0, result.getFailWidth() + result.getSkipWidth() + result.getPassWidth());
+  }
+
+  @Test
+  public void testNotModifiedTestResult() throws Exception {
+    SCTMTestResult result = new SCTMTestResult(dummyResult);
+    result.addSubTestCounts(1, 1, 1);
+    result.calculateBar();
+
+    Assert.assertEquals(33, result.getFailWidth());
+    Assert.assertEquals(33, result.getSkipWidth());
+    Assert.assertEquals(33, result.getPassWidth());
+    Assert.assertEquals(99, result.getFailWidth() + result.getSkipWidth() + result.getPassWidth());
+  }
+
+  @Test
+  public void testNotModifiedTestResultWithNoFaild() throws Exception {
+    SCTMTestResult result = new SCTMTestResult(dummyResult);
+    result.addSubTestCounts(80, 20, 0);
+    result.calculateBar();
+
+    Assert.assertEquals(0, result.getFailWidth());
+    Assert.assertEquals(20, result.getSkipWidth());
+    Assert.assertEquals(80, result.getPassWidth());
+    Assert.assertEquals(100, result.getFailWidth() + result.getSkipWidth() + result.getPassWidth());
+  }
+
+  @Test
+  public void testNotModifiedTestResultWithNoSkiped() throws Exception {
+    SCTMTestResult result = new SCTMTestResult(dummyResult);
+    result.addSubTestCounts(80, 0, 20);
+    result.calculateBar();
+
+    Assert.assertEquals(20, result.getFailWidth());
+    Assert.assertEquals(0, result.getSkipWidth());
+    Assert.assertEquals(80, result.getPassWidth());
+    Assert.assertEquals(100, result.getFailWidth() + result.getSkipWidth() + result.getPassWidth());
+  }
+
+  @Test
+  public void testNotModifiedTestResultWithNoPassed() throws Exception {
+    SCTMTestResult result = new SCTMTestResult(dummyResult);
+    result.addSubTestCounts(0, 20, 80);
+    result.calculateBar();
+
+    Assert.assertEquals(80, result.getFailWidth());
+    Assert.assertEquals(20, result.getSkipWidth());
+    Assert.assertEquals(0, result.getPassWidth());
+    Assert.assertEquals(100, result.getFailWidth() + result.getSkipWidth() + result.getPassWidth());
+  }
+
+  @Test
+  public void testModifiedFailWidth() throws Exception {
+    SCTMTestResult result = new SCTMTestResult(dummyResult);
+    result.addSubTestCounts(80, 19, 1);
+    result.calculateBar();
+
+    Assert.assertEquals(5, result.getFailWidth());
+    Assert.assertEquals(19, result.getSkipWidth());
+    Assert.assertEquals(76, result.getPassWidth());
+    Assert.assertEquals(100, result.getFailWidth() + result.getSkipWidth() + result.getPassWidth());
+  }
+
+  @Test
+  public void testModifiedFailWidth2() throws Exception {
+    SCTMTestResult result = new SCTMTestResult(dummyResult);
+    result.addSubTestCounts(108, 0, 1);
+    result.calculateBar();
+
+    Assert.assertEquals(5, result.getFailWidth());
+    Assert.assertEquals(0, result.getSkipWidth());
+    Assert.assertEquals(95, result.getPassWidth());
+    Assert.assertEquals(100, result.getFailWidth() + result.getSkipWidth() + result.getPassWidth());
+  }
+
+  @Test
+  public void testModifiedSkipWidth() throws Exception {
+    SCTMTestResult result = new SCTMTestResult(dummyResult);
+    result.addSubTestCounts(80, 1, 19);
+    result.calculateBar();
+
+    Assert.assertEquals(19, result.getFailWidth());
+    Assert.assertEquals(5, result.getSkipWidth());
+    Assert.assertEquals(76, result.getPassWidth());
+    Assert.assertEquals(100, result.getFailWidth() + result.getSkipWidth() + result.getPassWidth());
+  }
+
+  @Test
+  public void testModifiedPassWidth() throws Exception {
+    SCTMTestResult result = new SCTMTestResult(dummyResult);
+    result.addSubTestCounts(1, 80, 19);
+    result.calculateBar();
+
+    Assert.assertEquals(19, result.getFailWidth());
+    Assert.assertEquals(76, result.getSkipWidth());
+    Assert.assertEquals(5, result.getPassWidth());
+    Assert.assertEquals(100, result.getFailWidth() + result.getSkipWidth() + result.getPassWidth());
+  }
+
+  @Test
+  public void testMultibleAddSubTests() throws Exception {
+    SCTMTestResult result = new SCTMTestResult(dummyResult);
+    result.addSubTestCounts(1, 1, 1);
+    result.calculateBar();
+    Assert.assertEquals(33, result.getFailWidth());
+    Assert.assertEquals(33, result.getSkipWidth());
+    Assert.assertEquals(33, result.getPassWidth());
+    Assert.assertEquals(99, result.getFailWidth() + result.getSkipWidth() + result.getPassWidth());
+
+    result.addSubTestCounts(0, 39, 10);
+    result.addSubTestCounts(0, 1, 0);
+    result.addSubTestCounts(0, 30, 0);
+    result.addSubTestCounts(0, 9, 8);
+    result.calculateBar();
+    Assert.assertEquals(19, result.getFailWidth());
+    Assert.assertEquals(76, result.getSkipWidth());
+    Assert.assertEquals(5, result.getPassWidth());
+    Assert.assertEquals(100, result.getFailWidth() + result.getSkipWidth() + result.getPassWidth());
+
+  }
+}
