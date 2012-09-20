f2979a19bc4ddf0fecad456f7e4873b95115246e
kohsuke <kohsuke@dev.java.net>
2012-09-20 20:46:18 +0000

    Moved to GitHub

--- src/main/java/hudson/plugins/sctmexecutor/ITestResultWriter.java
+++
@@ -1,9 +0,0 @@
-package hudson.plugins.sctmexecutor;
-
-import hudson.plugins.sctmexecutor.exceptions.SCTMException;
-
-import com.borland.sctm.ws.execution.entities.ExecutionResult;
-
-public interface ITestResultWriter {
-  void write(ExecutionResult result) throws SCTMException;
-}
--- src/main/java/hudson/plugins/sctmexecutor/exceptions/SCTMException.java
+++
@@ -1,9 +0,0 @@
-package hudson.plugins.sctmexecutor.exceptions;
-
-public class SCTMException extends Exception {
-  private static final long serialVersionUID = 1L;
-
-  public SCTMException(String msg) {
-    super(msg);
-  }
-}
